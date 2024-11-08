b1406f44c239d2bdc9b5c23b20d8c902c6704873
Samarth Upare <samarth.upare@redtra.com>
2024-11-08 22:49:33 +0530

    investor doctype ui added

---
+++ finance_car/finance_car/doctype/investor/test_investor.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2024, Redtra Technologies FZE LLC and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests import IntegrationTestCase, UnitTestCase
+
+
+class TestInvestor(UnitTestCase):
+	"""
+	Unit tests for Investor.
+	Use this class for testing individual functions and methods.
+	"""
+
+	pass
+
+
+class TestInvestor(IntegrationTestCase):
+	"""
+	Integration tests for Investor.
+	Use this class for testing interactions between multiple components.
+	"""
+
+	pass
